0b00614fd9f669dc4c6522414dbd6e7aff24da40
Aleksi Saxlund <aleksi.saxlund@gmail.com>
2022-12-19 12:11:37 +0200

    Added value error exceptions and minor print improvements.

--- Protein_calculator.py
+++ Protein_calculator.py
@@ -1,16 +1,26 @@
 while True:
-    price = float(input("Price per kg: "))
-    protein_in_100g = float(input("Protein in 100g: "))
+    try:
+        price = float(input("Price per kg: "))
+        protein_in_100g = float(input("Protein in 100g: "))
+        
+        protein_in_100g /= 100
+        price /= 10 #per 100g hinta
+        protein_price_per_100g = price / protein_in_100g
 
-    protein_in_100g /= 100
-    price /= 10 #per 100g hinta
-    protein_price_per_100g = price / protein_in_100g
+    except ValueError:
+        print()
+        print("Please input whole- or decimal numbers using dots.")
+        print()
+        continue
 
     print()
     print(f"Protein price per 100g: {protein_price_per_100g:.2f}")
     print()
 
-    again = input("Again? y/n ")
-    
+    print("Again? y/n ")
+    again = input()
+
     if again == "n":
         break
+
+    print()
